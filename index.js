9de9cdf7d36d095e5a97fad74f48c623ae86abf7
H3rsk4 <80672298+H3rsk4@users.noreply.github.com>
2021-10-06 12:26:41 +0300

    Login and authentication implemented

--- index.js
+++ index.js
@@ -2,9 +2,72 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const passport = require('passport');
+const BasicStrategy = require('passport-http').BasicStrategy;
+
+app.use(express.json())
+
+const bcrypt = require('bcryptjs');
+
+let userDatabase = [];
+
+passport.use(new BasicStrategy(
+  (username, password, done) =>{
+    console.log('Basic strategy params, username: ' + username + ' password: ' + password);
+    //done(null, false);  //no match
+
+    const searchResult = userDatabase.find(user => {
+      //(username === user.username) && (password === user.password)
+      if(user.username === username) {
+        if(bcrypt.compareSync(password, user.password)){
+          return true;
+        }
+      }
+      return false;
+    });
+
+    if(searchResult != undefined){
+      done(null, searchResult);
+    }else{
+      done(null, false);
+    }
+    
+  }
+));
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/protectedResource', passport.authenticate('basic', { session: false }), (req, res) => {
+  //this api resource is now protected with HTTP Basic Authentication
+
+  res.send('Authorization successful');
+})
+
+
+/* this route will receive data structure:
+{
+  "username": "foo"
+  "password": "bar"
+  "email": "foo@bar.com"
+}
+*/
+app.post('/users', (req, res) => {
+
+  const salt = bcrypt.genSaltSync(6);
+  const hashedPassword = bcrypt.hashSync(req.body.password, salt)
+
+  const newUser = {
+    username: req.body.username,
+    password: hashedPassword,
+    email: req.body.email
+  }
+
+  userDatabase.push(newUser);
+  res.sendStatus(201);
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
