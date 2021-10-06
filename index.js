122a9072394327ddef187a6d2553066ec46d4e03
H3rsk4 <80672298+H3rsk4@users.noreply.github.com>
2021-10-06 14:50:55 +0300

    delete user; modify user

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ app.use(express.json())
 
 const bcrypt = require('bcryptjs');
 
+const { v4: uuidv4 } = require('uuid');
+
 let userDatabase = [];
+let postDatabase = [];
 
 passport.use(new BasicStrategy(
   (username, password, done) =>{
-    console.log('Basic strategy params, username: ' + username + ' password: ' + password);
     //done(null, false);  //no match
-
     const searchResult = userDatabase.find(user => {
       //(username === user.username) && (password === user.password)
       if(user.username === username) {
@@ -27,6 +28,7 @@ passport.use(new BasicStrategy(
     });
 
     if(searchResult != undefined){
+      //console.log(searchResult)
       done(null, searchResult);
     }else{
       done(null, false);
@@ -39,10 +41,74 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/protectedResource', passport.authenticate('basic', { session: false }), (req, res) => {
-  //this api resource is now protected with HTTP Basic Authentication
+app.get('/users/:id', passport.authenticate('basic', { session: false }), (req, res) => {
+  //this api resource is protected with HTTP Basic Authentication
 
-  res.send('Authorization successful');
+  //we only show the user if it matches with ours
+  if(req.params.id === req.user.userId){
+    const user = userDatabase.find(u => u.userId === req.params.id)
+    if(user === undefined){
+      res.sendStatus(404);
+    }else{
+      res.json(user);
+    }
+  }else{
+    res.sendStatus(401)
+  }
+  
+  //res.send('Authorization successful');
+})
+
+app.put('/users/:id', passport.authenticate('basic', { session: false }), (req, res) => {
+  //this api resource is protected with HTTP Basic Authentication
+
+  //we only show the user if it matches with ours
+  if(req.params.id === req.user.userId){
+    user = userDatabase.find(u => u.userId === req.params.id)
+    if(user === undefined){
+        res.sendStatus(404);
+    }else{
+      
+      const salt = bcrypt.genSaltSync(6);
+      const hashedPassword = bcrypt.hashSync(req.body.password, salt)
+      
+
+      user.username = req.body.username;
+      user.password = hashedPassword;
+      user.email = req.body.email;
+      //console.log(user.username)
+
+      res.sendStatus(200);
+    }
+  }else{
+    res.sendStatus(401)
+  }
+  
+  //res.send('Authorization successful');
+})
+
+app.delete('/users/:id', passport.authenticate('basic', { session: false }), (req, res) => {
+  //this api resource is protected with HTTP Basic Authentication
+
+  //we only show the user if it matches with ours
+  if(req.params.id === req.user.userId){
+    const user = userDatabase.find(u => u.userId === req.params.id)
+    if(user === undefined){
+        res.sendStatus(404);
+    }else{
+      var index = userDatabase.indexOf(user);
+      userDatabase.splice(index, 1);
+      res.sendStatus(200);
+    }
+  }else{
+    res.sendStatus(401)
+  }
+  
+})
+
+//this is for debugging purposes only
+app.get('/users', (req, res) => {
+  res.json(userDatabase);
 })
 
 
@@ -59,15 +125,27 @@ app.post('/users', (req, res) => {
   const hashedPassword = bcrypt.hashSync(req.body.password, salt)
 
   const newUser = {
+    userId: uuidv4(),
     username: req.body.username,
     password: hashedPassword,
     email: req.body.email
   }
 
   userDatabase.push(newUser);
+  //res.send(newUser.userId);
   res.sendStatus(201);
 })
 
+
+
+app.get('/posts', (req, res) => {
+  res.json(postDatabase);
+})
+
+app.post('/posts', passport.authenticate('basic', { session: false }), (req, res) => {
+  //this api resource is protected with HTTP Basic Authentication
+  //res.send('Authorization successful');
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
